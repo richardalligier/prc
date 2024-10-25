65979b99779f93c7f50d19cc9e5c5f5a8d4d4e19
Richard ALLIGIER <richard.alligier@enac.fr>
2024-10-25 09:54:14 +0200

    a

--- add_localtime.py
+++ add_localtime.py
@@ -1,15 +1,16 @@
 import pandas as pd
 from timezonefinder import TimezoneFinder
 
-def add_localtime(af,df,tadep,tades):
+def add_localtime(airports,flights,tadep,tades):
+    '''adds local_time date at adep and ades computed using the @airports, the utc times @tadep @tades in @flights '''
     l = [(tadep,"adep"),
      (tades,"ades")]
     for (t,a) in l:
-        df[t] = pd.to_datetime(df[t])
-        df = df.join(af[["icao_code","time_zone"]].set_index("icao_code"),on=a)
-        df = df.rename(columns={"time_zone":"time_zone_"+a})
-        df[f"local_{t}"]=(pd.to_datetime(df.groupby(f'time_zone_{a}')[f'{t}']
+        flights[t] = pd.to_datetime(flights[t])
+        flights = flights.join(airports[["icao_code","time_zone"]].set_index("icao_code"),on=a)
+        flights = flights.rename(columns={"time_zone":"time_zone_"+a})
+        flights[f"local_{t}"]=(pd.to_datetime(flights.groupby(f'time_zone_{a}')[f'{t}']
                                          .transform(lambda x: x.dt.tz_convert(x.name).dt.tz_localize(tz=None))
         )
         )
-    return df
+    return flights
